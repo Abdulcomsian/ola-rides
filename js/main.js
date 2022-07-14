b89079b483603b543965b7eb246b53930487d8a0
hashirAccrual <hashir.khan@accrualhub.com>
2022-07-14 12:24:41 +0500

    files/uploaded

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// init swiper
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + '0' + (index + 1) + "</span>";
+      },
+    },
+  });
