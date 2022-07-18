84a9e8cb5c21fc861b2cc64c707ae0c294ddfd96
hashirAccrual <hashir.khan@accrualhub.com>
2022-07-18 13:02:51 +0500

    updated

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ var swiper = new Swiper(".mySwiper.common", {
         return '<span class="' + className + '">' + '0' + (index + 1) + "</span>";
       },
     },
+    loop: true,
   });
 
   var swiper = new Swiper(".section_6 .mySwiper", {
@@ -21,4 +22,23 @@ var swiper = new Swiper(".mySwiper.common", {
       el: ".swiper-pagination",
       clickable: true,
     },
+    loop: true,
   });
+
+[...document.querySelectorAll('.btn_navgator.btn_prev')].forEach(elem => {
+  elem.addEventListener('click', (e) => {
+    e.preventDefault();
+    [...document.querySelectorAll('.swiper-button-prev')].forEach(elem2 => {
+      elem2.click()
+    })
+  })
+});
+
+[...document.querySelectorAll('.btn_navgator.btn_next')].forEach(elem => {
+  elem.addEventListener('click', (e) => {
+    e.preventDefault();
+    [...document.querySelectorAll('.swiper-button-next')].forEach(elem2 => {
+      elem2.click()
+    })
+  })
+})
